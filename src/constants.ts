4b00368f663b0d47001987eb1e74cf85d1d9015a
Fwy13 <quyhuynh1309@gmail.com>
2024-10-13 17:54:27 +0700

    chore: update url

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const url = import.meta.env.VITE_URL;
+export const url = "https://script.google.com/macros/s/AKfycby1E5y2WArG-4qLSLqss6CCixGinw2XWD4f1PIV2mwqGcyNuh7-c9XZ6es3eeoHYUiCHQ/exec";
