157b0eab8cc0ec4736222eb61718c31d28b2bf36
Fwy13 <quyhuynh1309@gmail.com>
2024-10-29 21:32:54 +0700

    fix: add new url

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const url = "https://script.google.com/macros/s/AKfycby1E5y2WArG-4qLSLqss6CCixGinw2XWD4f1PIV2mwqGcyNuh7-c9XZ6es3eeoHYUiCHQ/exec";
+export const url = "https://script.google.com/macros/s/AKfycbz4YAQFz4Wn3qzk5oHj6Ntxdfrs9CIwvXfXib3NNn3g2LCHlGPdRQKO7RRMenGb0r4jYQ/exec";
