322a4b53f5be0260540250a51d4e3dee1681a558
Fwy13 <quyhuynh1309@gmail.com>
2024-10-29 21:43:42 +0700

    chore: add new url

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const url = "https://script.google.com/macros/s/AKfycbwm0CB2cjJjPkTF7vKf7MNpFLUqfGY43ZUJI9130viQCWSspMgg0Byid1blh-k_Sovz7Q/exec";
+export const url = "https://script.google.com/macros/s/AKfycbwm0CB2cjJjPkTF7vKf7MNpFLUqfGY43ZUJI9130viQCWSspMgg0Byid1blh-k_Sovz7Q/exec"
